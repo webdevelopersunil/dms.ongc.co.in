ac7fc266fc254b5afa1cba1ee5aa4b545ee4c79d
Sreenath Sivadas <sreenathsdas@gmail.com>
2019-11-26 16:23:12 +0530

    composer autoload --optimize

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,53 +7,58 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function online() {
-        
+    public function online()
+    {
+
         $users = User::all();
 
-        return view('reports.users', compact('users')) ;
+        return view('reports.users', compact('users'));
     }
 
-    public function index() {
+    public function index()
+    {
         $users = User::all();
-        return view('user.index', compact('users') );
+        return view('user.index', compact('users'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('user.create');
     }
 
-    public function store(Request $request) {
-        
+    public function store(Request $request)
+    {
+
         $request->validate([
             'name' => 'required',
             'email' => 'required',
             'password' => 'required',
         ]);
-        
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => $request->password,
+            'password' => bcrypt($request->password),
         ]);
 
         return redirect()->back()->with('success', "User $user->name succesfully created");
     }
 
-    public function show($id) {
+    public function show($id)
+    {
         $user = User::findOrFail($id);
-        
+
         return $user;
     }
 
-    public function update(Request $request, $id) {
+    public function update(Request $request, $id)
+    { }
 
-    }
-
-    public function destroy($id) {
+    public function destroy($id)
+    {
         $user = User::findOrFail($id);
         $user->delete();
-        
-        return redirect()->back()->with('success', "User $user->name deleted!" );
+
+        return redirect()->back()->with('success', "User $user->name deleted!");
     }
 }
